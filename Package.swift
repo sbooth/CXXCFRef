d1348a22b14c31479cd82e4cb9bb947fd990732d
Stephen Booth <me@sbooth.org>
2026-01-25 10:57:08 -0600

    Update `cSettings` to `cxxSettings` (#21)

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
     targets: [
         .target(
             name: "CXXCFRef",
-            cSettings: [
+            cxxSettings: [
                 .headerSearchPath("include/cf"),
             ]
         ),
