658c0fbb661df4ba0e9c3f102627e8fadaa7fca7
Stephen Booth <me@sbooth.org>
2026-01-27 11:53:43 -0600

    Use most LLVM defaults

--- Sources/CXXCFRef/include/cf/CFRef.hpp
+++ Sources/CXXCFRef/include/cf/CFRef.hpp
@@ -72,21 +72,21 @@ template <typename T> class CFRef final {
 
     /// Constructs a copy of an existing CFRef.
     /// @param other A CFRef object.
-    CFRef(const CFRef& other) noexcept;
+    CFRef(const CFRef &other) noexcept;
 
     /// Replaces the managed object with the managed object from another CFRef.
     /// @param other A CFRef object.
     /// @return A reference to this.
-    CFRef& operator=(const CFRef& other) noexcept;
+    CFRef &operator=(const CFRef &other) noexcept;
 
     /// Constructs a CFRef by moving an existing CFRef.
     /// @param other A CFRef object.
-    CFRef(CFRef&& other) noexcept;
+    CFRef(CFRef &&other) noexcept;
 
     /// Replaces the managed object with the managed object from another CFRef.
     /// @param other A CFRef object.
     /// @return A reference to this.
-    CFRef& operator=(CFRef&& other) noexcept;
+    CFRef &operator=(CFRef &&other) noexcept;
 
     /// Destroys the CFRef and releases the managed object.
     ~CFRef() noexcept;
@@ -104,7 +104,7 @@ template <typename T> class CFRef final {
     /// Null objects are considered equal; non-null objects are compared using CFEqual.
     /// @param other A CFRef object.
     /// @return true if the objects are equal, false otherwise.
-    [[nodiscard]] bool isEqual(const CFRef& other) const noexcept;
+    [[nodiscard]] bool isEqual(const CFRef &other) const noexcept;
 
     /// Returns true if the managed object is equal to a CFTypeRef.
     ///
@@ -115,7 +115,7 @@ template <typename T> class CFRef final {
 
     /// Returns the managed object.
     /// @return A Core Foundation object or null.
-    [[nodiscard, clang::cf_returns_not_retained]] T _Nullable get() const& noexcept;
+    [[nodiscard, clang::cf_returns_not_retained]] T _Nullable get() const & noexcept;
 
     /// Resets the managed object and returns a pointer to the internal storage.
     ///
@@ -131,7 +131,7 @@ template <typename T> class CFRef final {
 
     /// Swaps the managed object with the managed object from another CFRef.
     /// @param other A CFRef object.
-    void swap(CFRef& other) noexcept;
+    void swap(CFRef &other) noexcept;
 
     /// Relinquishes ownership of the managed object and returns it.
     ///
@@ -139,7 +139,7 @@ template <typename T> class CFRef final {
     /// @return A Core Foundation object or null.
     [[nodiscard, clang::cf_returns_retained]] T _Nullable leak() noexcept;
 
-    T _Nullable get() const&&      = delete;
+    T _Nullable get() const && = delete;
     T _Nullable *_Nonnull put() && = delete;
 
   private:
@@ -151,9 +151,7 @@ template <typename T> class CFRef final {
 
 // MARK: Factory Methods
 
-template <typename T> inline auto CFRef<T>::adopt(T _Nullable object) noexcept -> CFRef {
-    return CFRef(object);
-}
+template <typename T> inline auto CFRef<T>::adopt(T _Nullable object) noexcept -> CFRef { return CFRef(object); }
 
 template <typename T> inline auto CFRef<T>::retain(T _Nullable object) noexcept -> CFRef {
     return CFRef(object, cf::retain);
@@ -163,47 +161,35 @@ template <typename T> inline auto CFRef<T>::retain(T _Nullable object) noexcept 
 
 template <typename T> inline CFRef<T>::CFRef(std::nullptr_t) noexcept {}
 
-template <typename T>
-inline CFRef<T>::CFRef(T _Nullable object) noexcept
-  : object_{object} {}
+template <typename T> inline CFRef<T>::CFRef(T _Nullable object) noexcept : object_{object} {}
 
 template <typename T>
 inline CFRef<T>::CFRef(T _Nullable object, retain_t /*unused*/) noexcept
-  : object_{object ? static_cast<T>(CFRetain(object)) : nullptr} {}
+        : object_{object ? static_cast<T>(CFRetain(object)) : nullptr} {}
 
-template <typename T>
-inline CFRef<T>::CFRef(const CFRef& other) noexcept
-  : CFRef(other.object_, cf::retain) {}
+template <typename T> inline CFRef<T>::CFRef(const CFRef &other) noexcept : CFRef(other.object_, cf::retain) {}
 
-template <typename T> inline auto CFRef<T>::operator=(const CFRef& other) noexcept -> CFRef& {
+template <typename T> inline auto CFRef<T>::operator=(const CFRef &other) noexcept -> CFRef & {
     reset(other.object_ ? static_cast<T>(CFRetain(other.object_)) : nullptr);
     return *this;
 }
 
-template <typename T>
-inline CFRef<T>::CFRef(CFRef&& other) noexcept
-  : object_{other.leak()} {}
+template <typename T> inline CFRef<T>::CFRef(CFRef &&other) noexcept : object_{other.leak()} {}
 
-template <typename T> inline auto CFRef<T>::operator=(CFRef&& other) noexcept -> CFRef& {
+template <typename T> inline auto CFRef<T>::operator=(CFRef &&other) noexcept -> CFRef & {
     reset(other.leak());
     return *this;
 }
 
-template <typename T> inline CFRef<T>::~CFRef() noexcept {
-    reset();
-}
+template <typename T> inline CFRef<T>::~CFRef() noexcept { reset(); }
 
 // MARK: Core Foundation Object Management
 
-template <typename T> inline CFRef<T>::operator bool() const noexcept {
-    return object_ != nullptr;
-}
+template <typename T> inline CFRef<T>::operator bool() const noexcept { return object_ != nullptr; }
 
-template <typename T> inline CFRef<T>::operator T() const noexcept {
-    return object_;
-}
+template <typename T> inline CFRef<T>::operator T() const noexcept { return object_; }
 
-template <typename T> inline bool CFRef<T>::isEqual(const CFRef& other) const noexcept {
+template <typename T> inline bool CFRef<T>::isEqual(const CFRef &other) const noexcept {
     return isEqual(static_cast<CFTypeRef>(other.object_));
 }
 
@@ -212,9 +198,7 @@ template <typename T> inline bool CFRef<T>::isEqual(CFTypeRef _Nullable other) c
            (object_ && (other != nullptr) && CFEqual(static_cast<CFTypeRef>(object_), other));
 }
 
-template <typename T> inline T _Nullable CFRef<T>::get() const& noexcept {
-    return object_;
-}
+template <typename T> inline T _Nullable CFRef<T>::get() const & noexcept { return object_; }
 
 template <typename T> inline T _Nullable *_Nonnull CFRef<T>::put() & noexcept {
     reset();
@@ -227,70 +211,66 @@ template <typename T> inline void CFRef<T>::reset(T _Nullable object) noexcept {
     }
 }
 
-template <typename T> inline void CFRef<T>::swap(CFRef& other) noexcept {
-    std::swap(object_, other.object_);
-}
+template <typename T> inline void CFRef<T>::swap(CFRef &other) noexcept { std::swap(object_, other.object_); }
 
-template <typename T> inline T _Nullable CFRef<T>::leak() noexcept {
-    return std::exchange(object_, nullptr);
-}
+template <typename T> inline T _Nullable CFRef<T>::leak() noexcept { return std::exchange(object_, nullptr); }
 
 // MARK: - Common Core Foundation Types
 
-using CFAllocator               = CFRef<CFAllocatorRef>;
-using CFArray                   = CFRef<CFArrayRef>;
-using CFAttributedString        = CFRef<CFAttributedStringRef>;
-using CFBag                     = CFRef<CFBagRef>;
-using CFBinaryHeap              = CFRef<CFBinaryHeapRef>;
-using CFBitVector               = CFRef<CFBitVectorRef>;
-using CFBoolean                 = CFRef<CFBooleanRef>;
-using CFBundle                  = CFRef<CFBundleRef>;
-using CFCalendar                = CFRef<CFCalendarRef>;
-using CFCharacterSet            = CFRef<CFCharacterSetRef>;
-using CFData                    = CFRef<CFDataRef>;
-using CFDate                    = CFRef<CFDateRef>;
-using CFDateFormatter           = CFRef<CFDateFormatterRef>;
-using CFDictionary              = CFRef<CFDictionaryRef>;
-using CFError                   = CFRef<CFErrorRef>;
-using CFFileDescriptor          = CFRef<CFFileDescriptorRef>;
-using CFFileSecurity            = CFRef<CFFileSecurityRef>;
-using CFLocale                  = CFRef<CFLocaleRef>;
-using CFMachPort                = CFRef<CFMachPortRef>;
-using CFMessagePort             = CFRef<CFMessagePortRef>;
-using CFMutableArray            = CFRef<CFMutableArrayRef>;
+using CFAllocator = CFRef<CFAllocatorRef>;
+using CFArray = CFRef<CFArrayRef>;
+using CFAttributedString = CFRef<CFAttributedStringRef>;
+using CFBag = CFRef<CFBagRef>;
+using CFBinaryHeap = CFRef<CFBinaryHeapRef>;
+using CFBitVector = CFRef<CFBitVectorRef>;
+using CFBoolean = CFRef<CFBooleanRef>;
+using CFBundle = CFRef<CFBundleRef>;
+using CFCalendar = CFRef<CFCalendarRef>;
+using CFCharacterSet = CFRef<CFCharacterSetRef>;
+using CFData = CFRef<CFDataRef>;
+using CFDate = CFRef<CFDateRef>;
+using CFDateFormatter = CFRef<CFDateFormatterRef>;
+using CFDictionary = CFRef<CFDictionaryRef>;
+using CFError = CFRef<CFErrorRef>;
+using CFFileDescriptor = CFRef<CFFileDescriptorRef>;
+using CFFileSecurity = CFRef<CFFileSecurityRef>;
+using CFLocale = CFRef<CFLocaleRef>;
+using CFMachPort = CFRef<CFMachPortRef>;
+using CFMessagePort = CFRef<CFMessagePortRef>;
+using CFMutableArray = CFRef<CFMutableArrayRef>;
 using CFMutableAttributedString = CFRef<CFMutableAttributedStringRef>;
-using CFMutableBag              = CFRef<CFMutableBagRef>;
-using CFMutableBitVector        = CFRef<CFMutableBitVectorRef>;
-using CFMutableCharacterSet     = CFRef<CFMutableCharacterSetRef>;
-using CFMutableData             = CFRef<CFMutableDataRef>;
-using CFMutableDictionary       = CFRef<CFMutableDictionaryRef>;
-using CFMutableSet              = CFRef<CFMutableSetRef>;
-using CFMutableString           = CFRef<CFMutableStringRef>;
-using CFNotificationCenter      = CFRef<CFNotificationCenterRef>;
-using CFNull                    = CFRef<CFNullRef>;
-using CFNumber                  = CFRef<CFNumberRef>;
-using CFNumberFormatter         = CFRef<CFNumberFormatterRef>;
-using CFPlugIn                  = CFRef<CFPlugInRef>;
-using CFPlugInInstance          = CFRef<CFPlugInInstanceRef>;
-using CFPropertyList            = CFRef<CFPropertyListRef>;
-using CFReadStream              = CFRef<CFReadStreamRef>;
-using CFRunLoop                 = CFRef<CFRunLoopRef>;
-using CFRunLoopObserver         = CFRef<CFRunLoopObserverRef>;
-using CFRunLoopSource           = CFRef<CFRunLoopSourceRef>;
-using CFRunLoopTimer            = CFRef<CFRunLoopTimerRef>;
-using CFSet                     = CFRef<CFSetRef>;
-using CFSocket                  = CFRef<CFSocketRef>;
-using CFString                  = CFRef<CFStringRef>;
-using CFStringTokenizer         = CFRef<CFStringTokenizerRef>;
-using CFTimeZone                = CFRef<CFTimeZoneRef>;
-using CFTree                    = CFRef<CFTreeRef>;
-using CFURL                     = CFRef<CFURLRef>;
-using CFUserNotification        = CFRef<CFUserNotificationRef>;
-using CFURLEnumerator           = CFRef<CFURLEnumeratorRef>;
-using CFUUID                    = CFRef<CFUUIDRef>;
-using CFWriteStream             = CFRef<CFWriteStreamRef>;
-using CFXMLNode                 = CFRef<CFXMLNodeRef>;
-using CFXMLParser               = CFRef<CFXMLParserRef>;
-using CFXMLTree                 = CFRef<CFXMLTreeRef>;
+using CFMutableBag = CFRef<CFMutableBagRef>;
+using CFMutableBitVector = CFRef<CFMutableBitVectorRef>;
+using CFMutableCharacterSet = CFRef<CFMutableCharacterSetRef>;
+using CFMutableData = CFRef<CFMutableDataRef>;
+using CFMutableDictionary = CFRef<CFMutableDictionaryRef>;
+using CFMutableSet = CFRef<CFMutableSetRef>;
+using CFMutableString = CFRef<CFMutableStringRef>;
+using CFNotificationCenter = CFRef<CFNotificationCenterRef>;
+using CFNull = CFRef<CFNullRef>;
+using CFNumber = CFRef<CFNumberRef>;
+using CFNumberFormatter = CFRef<CFNumberFormatterRef>;
+using CFPlugIn = CFRef<CFPlugInRef>;
+using CFPlugInInstance = CFRef<CFPlugInInstanceRef>;
+using CFPropertyList = CFRef<CFPropertyListRef>;
+using CFReadStream = CFRef<CFReadStreamRef>;
+using CFRunLoop = CFRef<CFRunLoopRef>;
+using CFRunLoopObserver = CFRef<CFRunLoopObserverRef>;
+using CFRunLoopSource = CFRef<CFRunLoopSourceRef>;
+using CFRunLoopTimer = CFRef<CFRunLoopTimerRef>;
+using CFSet = CFRef<CFSetRef>;
+using CFSocket = CFRef<CFSocketRef>;
+using CFString = CFRef<CFStringRef>;
+using CFStringTokenizer = CFRef<CFStringTokenizerRef>;
+using CFTimeZone = CFRef<CFTimeZoneRef>;
+using CFTree = CFRef<CFTreeRef>;
+using CFURL = CFRef<CFURLRef>;
+using CFUserNotification = CFRef<CFUserNotificationRef>;
+using CFURLEnumerator = CFRef<CFURLEnumeratorRef>;
+using CFUUID = CFRef<CFUUIDRef>;
+using CFWriteStream = CFRef<CFWriteStreamRef>;
+using CFXMLNode = CFRef<CFXMLNodeRef>;
+using CFXMLParser = CFRef<CFXMLParserRef>;
+using CFXMLTree = CFRef<CFXMLTreeRef>;
 
 } /* namespace cf */
